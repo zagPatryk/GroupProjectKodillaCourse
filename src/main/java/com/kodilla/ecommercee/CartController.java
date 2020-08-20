e222fc931d0e1b6db63418accd445a50811afb21
MsFaxe <anty.wjj@gmail.com>
2020-08-20 18:01:49 +0200

    task JDP200801-3 CartController - initial endpoinds with dummy data

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,39 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.CartDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/ecommerce/cart")
 public class CartController {
+    @RequestMapping(method = RequestMethod.GET, value = "createNewCart")
+    public CartDto createNewCart() {
+        return new CartDto(1L, new ArrayList<>());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProductsFromCart")
+    public List<String> getProductsFromCart(Long cartId) {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "addProductToCart", consumes = APPLICATION_JSON_VALUE)
+    public CartDto addProductToCart(/*@RequestBody*/ Long productId) {
+        return new CartDto(1L, Arrays.asList("product1"));
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProductFromCart", consumes = APPLICATION_JSON_VALUE)
+    public String deleteProductFromCart(/*@RequestParam*/ Long productId) {
+        return "item deleted";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createNewOrder", consumes = APPLICATION_JSON_VALUE)
+    public String createNewOrder(/*@RequestBody*/ Long cartId) {
+        return "New Order created";
+    }
 }
--- src/main/java/com/kodilla/ecommercee/domain/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -1,2 +1,13 @@
-package com.kodilla.ecommercee.domain;public class CartDto {
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class CartDto {
+    private Long id;
+    private List<String> cart; //List<Product>
 }
