251fee25db1b5919fc2f39a03b5dc26ef6fb75c6
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-08-20 19:14:08 +0200

    JDP200801-2: Implemented GroupController with  ProductGroupDto class to symulate endpoints.

--- src/main/java/com/kodilla/ecommercee/GenericEntityRepository.java
+++ src/main/java/com/kodilla/ecommercee/GenericEntityRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GenericEntityRepository extends JpaRepository<GenericEntity, Long> { }
+public interface GenericEntityRepository extends JpaRepository<GenericEntity, Long> {
+
+}
