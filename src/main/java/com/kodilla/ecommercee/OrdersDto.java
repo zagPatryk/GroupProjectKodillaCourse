e72236710d9a5bd28b7bbf4c6e04af0e444f2d3e
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-08-21 15:34:38 +0200

    JDP200801-2: Implemented GroupController with ProductGroupDto class to symulate endpoints, added gradle coretion

---
+++ src/main/java/com/kodilla/ecommercee/OrdersDto.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrdersDto {
+    public Long idOrder;
+    public String nameOfOrder;
+}
+
