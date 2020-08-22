2c0b8f4e6cddd6036c466362108f07c480244087
MsFaxe <anty.wjj@gmail.com>
2020-08-22 14:23:48 +0200

    task JDP200801-11: Order Entity - class & repository implementation

--- src/main/java/com/kodilla/ecommercee/domain/cart/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/cart/Cart.java
@@ -1,4 +1,18 @@
 package com.kodilla.ecommercee.domain.cart;
 
+import com.kodilla.ecommercee.domain.order.Order;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 public class Cart {
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "CART_ID")
+    private Long cartId;
+
+    @OneToOne
+    @JoinColumn(name = "ORDER_ID", referencedColumnName = "ORDER_ID")
+    private Order order;
 }
--- src/main/java/com/kodilla/ecommercee/domain/user/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/user/User.java
@@ -1,4 +1,12 @@
 package com.kodilla.ecommercee.domain.user;
 
+import com.kodilla.ecommercee.domain.order.Order;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
 public class User {
+    @OneToMany
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
 }
