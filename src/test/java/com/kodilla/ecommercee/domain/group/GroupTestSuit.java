600ec82df1b833daf86854915497b07fc8bddc4c
mysolution <mysolution@op.pl>
2020-08-24 23:28:57 +0200

    JDP200801 - testSaveGroup fixed to run v1

--- src/test/java/com/kodilla/ecommercee/domain/group/GroupTestSuit.java
+++ src/test/java/com/kodilla/ecommercee/domain/group/GroupTestSuit.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee.domain.group;
 
-import com.kodilla.ecommercee.data.CartEntity;
 import com.kodilla.ecommercee.domain.group.dao.GroupDao;
 import com.kodilla.ecommercee.domain.product.Product;
 import org.junit.Test;
@@ -11,7 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertTrue;
@@ -26,23 +26,43 @@ public class GroupTestSuit {
     @Test
     public void testSaveGroup() {
         // Given
-        CartEntity cartEntity = new CartEntity(1L, new ArrayList<Product>());
-        Product product = new Product(1L, "Product name", "Desc", 10, new ArrayList<CartEntity>(), new Group());
-        Group group = new Group(1L, "Name Group", new ArrayList<Product>());
+        Product product1 = new Product();
+        Product product2 = new Product();
+        Product product3 = new Product();
+        Product product4 = new Product();
+        Product product5 = new Product();
+        Product product6 = new Product();
+        Group group1 = new Group();
+        Group group2 = new Group();
 
-        group.getProducts().add(product);
-        product.getCarts().add(cartEntity);
-        cartEntity.getProductsList().add(product);
+        List<Product> productList1 = Arrays.asList(product1, product2, product3);
+        group1.setProducts(productList1);
+        product1.setGroupId(group1);
+        product2.setGroupId(group1);
+        product3.setGroupId(group1);
+
+        List<Product> productList2 = Arrays.asList(product4, product5, product6);
+        group2.setProducts(productList2);
+        product4.setGroupId(group2);
+        product5.setGroupId(group2);
+        product6.setGroupId(group2);
 
         // When
-        groupDao.save(group);
-        long id = group.getId();
+        groupDao.save(group1);
+        long id1 = group1.getId();
+
+        groupDao.save(group2);
+        long id2 = group2.getId();
 
         // Then
-        Optional<Group> groupIdDao = groupDao.findById(id);
-        assertTrue(groupIdDao.isPresent());
+        Optional<Group> groupIdDao1 = groupDao.findById(id1);
+        assertTrue(groupIdDao1.isPresent());
+
+        Optional<Group> groupIdDao2 = groupDao.findById(id2);
+        assertTrue(groupIdDao2.isPresent());
 
         // Clear up
-        groupDao.deleteById(id);
+        groupDao.deleteById(id1);
+        groupDao.deleteById(id2);
     }
 }
