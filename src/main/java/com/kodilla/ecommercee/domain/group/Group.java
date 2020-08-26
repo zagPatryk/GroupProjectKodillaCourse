fa5c863d06bd447ba84046336eecf2676217c942
Greem666 <29668023+Greem666@users.noreply.github.com>
2020-08-27 00:37:40 +1000

    Quickfix for table name, to address MySQL syntax issues

--- src/main/java/com/kodilla/ecommercee/domain/group/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/group/Group.java
@@ -15,7 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Entity
-@Table(name="GROUPS")
+@Table(name="`GROUP`")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Group {
     @Id
