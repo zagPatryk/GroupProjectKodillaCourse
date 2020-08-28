803b3e6d28795ce72d893054087ab76bac6c7126
MsFaxe <anty.wjj@gmail.com>
2020-08-28 18:15:38 +0200

    changes in OrderTestSuit 28.08

--- src/main/java/com/kodilla/ecommercee/domain/user/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/user/User.java
@@ -24,6 +24,7 @@ public class User {
     private Long userId;
 
     @OneToMany(
+            cascade = CascadeType.ALL,
             targetEntity = Order.class,
             mappedBy = "orderId"
     )
