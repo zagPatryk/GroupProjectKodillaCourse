b301c810862e627260ea8812ec8845089fab4c3f
krysiakpawel <krysiak.p@o2.pl>
2020-08-29 20:47:32 +0000

    User inital endpoints implementation

--- src/main/java/com/kodilla/ecommercee/domain/user/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/user/UserDto.java
@@ -1,4 +1,16 @@
 package com.kodilla.ecommercee.domain.user;
 
+import lombok.Getter;
+
+@Getter
 public class UserDto {
+
+    private Long id;
+    private Long userkey;
+    private String username;
+    private String password;
+    private String address;
+    private boolean isActive;
+
+
 }
