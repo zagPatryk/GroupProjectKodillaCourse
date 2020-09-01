a6f92ce214e64eafb385608fcb0cf28c17ea1af5
MsFaxe <anty.wjj@gmail.com>
2020-09-01 12:11:34 +0200

    task JDP200801-22 - GroupController methods implementation

--- src/main/java/com/kodilla/ecommercee/domain/group/dao/GroupDao.java
+++ src/main/java/com/kodilla/ecommercee/domain/group/dao/GroupDao.java
@@ -1,13 +1,25 @@
 package com.kodilla.ecommercee.domain.group.dao;
 
 import com.kodilla.ecommercee.domain.group.Group;
-import com.kodilla.ecommercee.domain.order.Order;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Repository
 public interface GroupDao extends CrudRepository<Group, Long> {
+    @Override
+    List<Group> findAll();
+
+    @Override
+    Optional<Group> findById(Long id);
+
+    @Override
+    Group save(Group group);
+
+    @Override
+    void deleteById(Long id);
 }
--- src/main/java/com/kodilla/ecommercee/service/GroupService.java
+++ src/main/java/com/kodilla/ecommercee/service/GroupService.java
@@ -1,4 +1,31 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.domain.group.Group;
+import com.kodilla.ecommercee.domain.group.dao.GroupDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class GroupService {
+    @Autowired
+    private GroupDao groupDao;
+
+    public List<Group> getAllGroups() {
+        return groupDao.findAll();
+    }
+
+    public Optional<Group> getGroup(final long id) {
+        return groupDao.findById(id);
+    }
+
+    public Group saveGroup(final Group group) {
+        return groupDao.save(group);
+    }
+
+    public void deleteGroup(long id) {
+        groupDao.deleteById(id);
+    }
 }
