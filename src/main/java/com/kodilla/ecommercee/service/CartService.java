e5d608ceb2999aff5af63890b03963b142ff844d
MarcinSkrzecz <marpiowawa@o2.pl>
2020-09-14 19:23:58 +0200

    Cart Controller 114.09.2020

--- src/main/java/com/kodilla/ecommercee/service/CartService.java
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -1,4 +1,65 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.domain.cart.Cart;
+import com.kodilla.ecommercee.domain.cart.CartDto;
+import com.kodilla.ecommercee.domain.cart.dao.CartDao;
+import com.kodilla.ecommercee.domain.order.OrderDto;
+import com.kodilla.ecommercee.domain.product.Product;
+import com.kodilla.ecommercee.domain.product.ProductDto;
+import com.kodilla.ecommercee.mapper.CartMapper;
+import com.kodilla.ecommercee.mapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class CartService {
+    @Autowired
+    private final CartDao cartDao;
+    @Autowired
+    private final CartMapper cartMapper;
+    @Autowired
+    private final OrderService orderService;
+    @Autowired
+    private final ProductMapper productMapper;
+    @Autowired
+    private final ProductService productService;
+    @Autowired
+    private final UserService userService;
+
+    public CartDto getNewCart(Long userId) {
+        Cart cart = new Cart(userService.getUser(userId));
+        cartDao.save(cart);
+        return cartMapper.mapToCartDto(cart);
+    }
+
+    public List<ProductDto> getAllProductsFromCart(Long cartId) {
+        Cart cart = cartDao.findById(cartId).orElse(new Cart());
+        return productMapper.mapToProductDto(cart.getProductsList());
+    }
+
+    public CartDto addProductToCart(Long cartId, Long productId) {
+        Cart cart = cartDao.findById(cartId).orElse(new Cart());
+        cart.addProduct(productService.getProduct(productId));
+        return cartMapper.mapToCartDto(cartDao.save(cart));
+    }
+
+    public boolean deleteSelectedProductFromCart(Long cartId, Long productId) {
+        if (cartDao.existsById(cartId)) {
+            Cart cart = cartDao.findById(cartId).orElse(new Cart());
+            Product productToRemove =productService.getProduct(productId);
+            getAllProductsFromCart(cartId).remove(productToRemove);
+            cartMapper.mapToCartDto(cartDao.save(cart));
+            return true;
+        } else {
+            throw new IllegalArgumentException("Cart not exist");
+        }
+    }
+
+    public OrderDto createOrder(CartDto cartDto) {
+        return orderService.createOrder(cartDto);
+    }
 }
